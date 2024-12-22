bb830603d6c6bec3e3370e268b4774b1c8303f37
KostianDev <verbytskyi.kostiantyn@lll.kpi.ua>
2024-12-22 02:47:38 +0200

    Add EJS

--- src/server.js
+++ src/server.js
@@ -1,8 +1,19 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import expressLayouts from 'express-ejs-layouts';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('layout', 'layouts/main');
+
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.render('index', { title: 'Travel master' });
 });
 
 const port = 3001;
